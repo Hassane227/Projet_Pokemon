ad6901ebc100da9163572ff69dba198920d644dc
Adamou Moussa Hassane <adamoumoussahassan@gmail.com>
2025-02-02 14:49:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {POKEMONS} from './mock-pokemon'
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  templateUrl:'./app.component.html',
   standalone: false,
   styles: []
 })
-export class AppComponent {
-  title = 'projetPokemon';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] =POKEMONS;
+  pokemonSelected : Pokemon|undefined;
+  ngOnInit(){
+    console.table(this.pokemonList)
+
+      
+  }
+
+  selectPokemon(pokemonid: String){
+const pokemon:Pokemon|undefined = this.pokemonList.find(pokemon=>pokemon.id==+pokemonid);
+
+if(pokemon){
+
+  console.log(`vous avez demander le pokemonc${pokemon.name}`);
+  this.pokemonSelected=pokemon;
+
+}
+else{
+  console.log(`vous avez demandé un pokemon qui n'existe pas`);
+  this.pokemonSelected=pokemon;
+
+}
+       
+  }
 }
